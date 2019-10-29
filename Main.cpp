591757056b2acfb52a25e50fea9386f0e84acb3c
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-29 16:09:26 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+int main() {
+  char input[80];
+  while (true) {
+    cout << "This is the Senjougahara arc of Bakemonogatari" << endl;
+    cout << "Are you ready?" << endl;
+    cin >> input;
+    if (stcmp(input, "Yes") == 0) {
+      crab();
+    }
+    else if (strcmp(input, "No") == 0) {
+      return false;
+    }
+    else {
+      cout << "That's an invalid answer" << endl;
+    }
+  }
+}
