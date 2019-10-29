a21eeb59ba9bf254d2683ceb5d6ea1fd721b04d9
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-29 16:51:50 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -27,19 +27,49 @@ int main() {
 }
 
 void crab(){
+  char next[80];
   cout<<"This is a tale of a crab and Hitagi Senjougahara"<<endl;
+  
+  //At the end
+  cout<<"Do you want to continue to the next arc?"<<endl;
+  cin>>next;
+  if (strcmp(next, "Yes")==0){
+    snail();
+  }
 }
 
 void snail(){
   cout<<"This is a tale of a snail and Hachikuji Mayoi"<<endl;
+  
+  
+  //At the end
+  cout<<"Do you want to continue to the next arc?"<<endl;
+  cin>>next;
+  if (strcmp(next, "Yes")==0){
+    monkey();
+  }
 }
 
 void monkey(){
   cout<<"This is a tale of a monkey and Suruga Kanbaru"<<endl;
+  
+  //At the end
+  cout<<"Do you want to continue to the next arc?"<<endl;
+  cin>>next;
+  if (strcmp(next, "Yes")==0){
+    snake();
+  }
 }
 
 void snake(){
   cout<<"This is a tale of a snake and Nadeko Sengoku"<<endl;
+  
+  //At the end
+  cout<<"Do you want to continue to the next arc?"<<endl;
+  cin>>next;
+  if (strcmp(next, "Yes")==0){
+    cat();
+  }
 }
 
 void cat(){
